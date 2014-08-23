122152680ca4b6a4e43d175f0d6dffd283718354
csharpd <chloedonegan@gmail.com>
2014-08-23 22:10:48 +0100

    Only logged in users can post

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -1,17 +1,30 @@
 require 'rails_helper'
 
 describe 'creating posts' do
-  it 'adds a text posts from a form' do
-    visit '/posts'
-    click_link 'New Post'
-    fill_in 'Title', with: 'Frogs'
-    fill_in 'Url', with: 'http://s.ngm.com/2006/11/tree-frogs/img/red-eyed-tree-frogs-615.jpg'
-    fill_in 'Description', with: 'Two little frogs'
-    click_button 'Create Post'
+  context 'if not logged in' do
+    it 'when a user clicks New Post, should be prompted to sign in' do
+      visit '/posts'
+      click_link 'New Post'
+      expect(page).to have_content 'Log in'
+    end
+  end
+  context 'when logged in' do
+    before do
+      chloe =create(:user)
+      login_as chloe
+    end
+    it 'adds a text posts from a form' do
+      visit '/posts'
+      click_link 'New Post'
+      fill_in 'Title', with: 'Frogs'
+      fill_in 'Url', with: 'http://s.ngm.com/2006/11/tree-frogs/img/red-eyed-tree-frogs-615.jpg'
+      fill_in 'Description', with: 'Two little frogs'
+      click_button 'Create Post'
 
-    expect(page).to have_content 'Frogs'
-    expect(page).to have_content 'http://s.ngm.com/2006/11/tree-frogs/img/red-eyed-tree-frogs-615.jpg'
-     expect(page).to have_content 'Two little frogs'
-    expect(current_path).to eq '/posts'
+      expect(page).to have_content 'Frogs'
+      expect(page).to have_content 'http://s.ngm.com/2006/11/tree-frogs/img/red-eyed-tree-frogs-615.jpg'
+       expect(page).to have_content 'Two little frogs'
+      expect(current_path).to eq '/posts'
+    end
   end
 end
